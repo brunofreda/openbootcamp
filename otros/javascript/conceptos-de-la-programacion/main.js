94521942dd927fbbac30654dd931cd828a316d61
brunofreda <brunodemianfreda@gmail.com>
2022-11-16 16:52:06 -0300

    fifth commit

---
+++ otros/javascript/conceptos-de-la-programacion/main.js
@@ -0,0 +1,81 @@
+/* const listaProgramadores = ["Gorka", "Martin", "Alex", "Leire"]
+
+// let nombres = []
+// listaProgramadores.forEach((programador, posicion) => {
+//     nombres[posicion] = programador
+// })
+
+// let nombres = [...listaProgramadores] */
+
+// let suma = 0
+//
+// for (let i = 1; i <= 10; i++) {
+//     suma = suma + i
+// }
+
+/* function diezEnteros() {
+     let suma = 0
+
+     for (let i = 1; i <= 10; i++) {
+         suma = suma + i
+     }
+
+     return "Hola mundo"
+ }
+
+ let suma = diezEnteros() */
+
+/* // function suma(num1, num2) {
+//     return num1 + num2
+// }
+
+// let total = 0
+// function suma(num1, num2) {
+//     total = total + num1 + num2
+//     return total
+// }
+
+console.log(suma(3, 8))
+console.log(suma(3, 8))
+console.log(suma(3, 8))
+console.log(suma(3, 8)) */
+
+/* // function factorial(num) {
+//     for(i = num - 1; i > 0; i--) {
+//         num *= i
+//     }
+//     return num
+// }
+
+// function factorial(num) {
+//     if (num === 1) return 1
+//     return num * factorial(num - 1)
+// }
+
+console.log(factorial(5))
+console.log(factorial(5)) */
+
+/* function suma(num1, num2) {
+    return num1 + num2
+}
+
+function multiplica(num1, num2) {
+    return num1 * num2
+}
+
+function sumaYMultiplica(num1, num2) {
+    return suma(num1, num2) * multiplica(num1, num2)
+}
+
+console.log(suma(1, 4))
+console.log(multiplica(1, 4))
+console.log(sumaYMultiplica(1, 4)) */
+
+/* // nombres = 1
+// nombres = "perro"
+// nombres = "11"
+
+console.log(typeof(nombres))
+console.log(11 + nombres)
+console.log(11 + "11")
+console.log(11 + 11) */
