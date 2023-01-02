888de7518fd635f4e49b2cd7dc9e93b6aa787f6f
brunofreda <brunodemianfreda@gmail.com>
2023-01-02 20:00:21 -0300

    twenty-first commit

--- python/ejercicio_6/ejercicio_6_1.py
+++ python/ejercicio_6/ejercicio_6_1.py
@@ -8,14 +8,25 @@
 # Por último, tendrás que crear un objeto de la clase Coche y mostrarlo por consola.
 
 class Vehiculo:
-    color = "Negro"
-    ruedas = 4
-    puertas = 5
+    def __init__(self, color, ruedas, puertas):
+        self.color = color
+        self.ruedas = ruedas
+        self.puertas = puertas
+
+    def __str__(self):
+        vehiculo = self.color + ", " + str(self.ruedas) + ", " + str(self.puertas)
+        return vehiculo
 
 class Coche(Vehiculo):
-    velocidad = 200
-    cilindrada = 4100
+    def __init__(self, color, ruedas, puertas, velocidad, cilindrada):
+        super().__init__(color, ruedas, puertas)
+        self.velocidad = velocidad
+        self.cilindrada = cilindrada
+    
+    def __str__(self):
+        vehiculo = self.color + ", " + str(self.ruedas) + ", " + str(self.puertas) + ", " + str(self.velocidad) + ", " + str(self.cilindrada) 
+        return vehiculo
 
-coche_1 = Coche()
+coche_1 = Coche("Negro", 4, 4, 200, 4100)
 
-print("coche_1 es de color ", coche_1.color.lower(), ", tiene ", coche_1.ruedas, " ruedas, ", coche_1.puertas, " puertas, una velocidad de ", coche_1.velocidad, " km por hora y una cilindrada de ", coche_1.cilindrada, " cc.", sep="")
+print(coche_1)
