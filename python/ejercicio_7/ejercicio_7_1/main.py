1e96db5a8032dc283dcda42040beb8cb01a7a024
brunofreda <brunodemianfreda@gmail.com>
2023-01-04 20:27:56 -0300

    twenty-second commit

---
+++ python/ejercicio_7/ejercicio_7_1/main.py
@@ -0,0 +1,19 @@
+import operaciones as o
+
+
+def main():
+    
+    suma = o.sumar(4, 2)
+    resta = o.restar(4, 2)
+    multiplicacion = o.multiplicar(4, 2)
+    division = o.dividir(4, 2)
+
+    print(suma)
+    print(resta)
+    print(multiplicacion)
+    print(division)
+
+
+
+if __name__ == "__main__":
+    main() 
